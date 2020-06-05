c5a94c2241009f33a10fc4271892c3d2cd65ed80
Patrick Shushereba <pshushereba@gmail.com>
2020-06-04 20:03:06 -0700

    Finished iterative sorting.

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -4,10 +4,10 @@ def selection_sort(arr):
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        # Your code here
+        # start a new count to compare each item to the current smallest_index
         for j in range(cur_index + 1, len(arr)):
+            # compare all these values to the value at cur_index
+            #find the smallest
             if arr[j] < arr[smallest_index]:
                 smallest_index = j
 
@@ -23,8 +23,17 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
+    made_a_swap = True
 
+    while made_a_swap:
+        made_a_swap = False
 
+        for i in range(0, len(arr) - 1):
+            j = i + 1
+
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+                made_a_swap = True
     return arr
 
 '''
@@ -46,6 +55,11 @@ What is the time and space complexity of the counting sort algorithm?
 '''
 def counting_sort(arr, maximum=None):
     # Your code here
+    counts = [0] * (maximum + 1)
 
+    for value in arr:
+        counts[value] += 1
+
+    
 
     return arr
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,29 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(0, len(arr)):
+        if arr[i] = target:
+            return i
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
+    if len(arr) == 0:
+        return -1
     # Your code here
 
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+    # go to the middle
+        middle = (left + right) // 2
+    # ask if the middle is less than or greater than our target
+
+    # if less, eliminate the right-hand side
+
+
+    # adjust the low or high accordingly
 
     return -1  # not found
